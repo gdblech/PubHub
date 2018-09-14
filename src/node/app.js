6fd75684ff95055ad2854455c51156a2787b276c
Travis Cox <tlcox3@uncg.edu>
2018-09-14 01:06:21 -0400

    #43 added get user api endpoint

--- src/node/app.js
+++ src/node/app.js
@@ -1,7 +1,15 @@
+const log4js = require('log4js');
+let logger = log4js.getLogger();
+logger.level = process.env.LOG_LEVEL;
+
 const express = require('express');
 const app = express();
 const port = 8081;
-
+const morgan = require('morgan');
+app.use(morgan('common'));
 app.get('/', (req, res) => res.send('Test'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+let userRoute = require('./routes/users.js');
+app.use('/api/user', userRoute);
+
+app.listen(port, () => logger.info(`Example app listening on port ${port}!`));
