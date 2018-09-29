c5794d32b6b895282096f8332bfde19f9c1307f3
Geoffrey B <gdblech@uncg.edu>
2018-09-28 21:17:13 -0400

    #45 Got google sign in working, more needs to be done but it works.

---
+++ src/app/app/src/main/java/com/example/blairgentry/pubhub_java/SignOut.java
@@ -0,0 +1,13 @@
+package com.example.blairgentry.pubhub_java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignOut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_out);
+    }
+}
