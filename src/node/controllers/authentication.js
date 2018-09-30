6f59971874c4e16c336ae07ac5cf860753a998ba
Travis Cox <tlcox3@uncg.edu>
2018-09-30 17:16:52 -0400

    #143 auth stuff

--- src/node/controllers/authentication.js
+++ src/node/controllers/authentication.js
@@ -2,7 +2,11 @@ const log4js = require('log4js');
 let logger = log4js.getLogger();
 
 let auth = async (req, res) => {
-	const User = require('../models/user');
+	const User = require('../models').User;
+	const Role = require('../models').Role;
+	const AuthType = require('../models').AuthType;
+
+	console.log(JSON.stringify(User));
 	const {
 		OAuth2Client
 	} = require('google-auth-library');
@@ -18,7 +22,38 @@ let auth = async (req, res) => {
 			audience: CLIENT_ID
 		});
 		const payload = ticket.getPayload();
-		const userid = payload['sub'];
+		const userId = payload['sub'];
+		const email = payload['email'];
+		const userName = payload['email'];
+		const profilePicPath = payload['picture'];
+		const customerRole = await Role.find({
+			where: {
+				roleName: 'Customer'
+			}
+		});
+		let googleAuth = await AuthType.find({
+			where: {
+				provider: 'google'
+			}
+		});
+
+		logger.debug(`role: ${customerRole.id}`);
+		let defaults = {
+			userId,
+			email,
+			userName,
+			profilePicPath
+		}
+		//logger.debug(JSON.stringify(defaults));
+		let user = await User.findOrCreate({
+			where: {
+				userId
+			},
+			defaults,
+		});
+
+		logger.debug(`user: ${JSON.stringify(user)}`);
+		res.send('done');
 	} catch (err) {
 		logger.error(err);
 	}
