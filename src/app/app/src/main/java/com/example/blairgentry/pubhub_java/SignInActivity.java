833afeef9b55c8059216dd7a6328c8281b3033c6
Geoffrey B <gdblech@uncg.edu>
2018-09-30 17:13:03 -0400

    #42 adjusted sign in code to me simpler
    Made code more in line with documentation and prepped it for authentication integeration

--- src/app/app/src/main/java/com/example/blairgentry/pubhub_java/SignInActivity.java
+++ src/app/app/src/main/java/com/example/blairgentry/pubhub_java/SignInActivity.java
@@ -5,24 +5,26 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 
-public class SignInActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
     private SignInButton signInButton;
-    private String iDToken;
-    private String email;
-    private GoogleApiClient googleApiClient;
+    private String idToken;
+    private String phbToken;
+    private GoogleSignInClient googleSignInClient;
     private static final int REQ_CODE = 13374;
+    private static final String TAG = "SignInActivity";
 
 
     @Override
@@ -37,56 +39,67 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         //sign in variables
         GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.server_client_id))
-                .requestEmail()
                 .build();
 
-        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,
-                this).addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions).build();
+        googleSignInClient = GoogleSignIn.getClient(this, signInOptions);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //If the user has already signed in previously, sign them in without the button, and so silently
+        googleSignInClient.silentSignIn()
+                .addOnCompleteListener(this, new OnCompleteListener<GoogleSignInAccount>() {
+                    @Override
+                    public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                        handleSignInResult(task);
+                    }
+                });
     }
 
     @Override
     public void onClick(View view) {
-        signIn();
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    private void signIn() {
-        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-        startActivityForResult(intent, REQ_CODE);
-    }
-
-    private void signOut() {
-
-    }
-
-    private void handleResult(GoogleSignInResult result) {
-        if(result.isSuccess()){
-            GoogleSignInAccount account = result.getSignInAccount();
-            iDToken = account.getIdToken();
-            email = account.getEmail();
-            updateUI(true);
+        switch(view.getId()){
+            case R.id.sign_in_button:
+                signIn();
+                break;
         }
     }
 
-    private void updateUI(boolean hasLogedIn) {
-        if(hasLogedIn){
-            System.out.println("Log in worked");
-        }else{
-            //print out error and have a popup about it
+    private void signIn(){
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(signInIntent);
+        handleSignInResult(task);
+    }
+    private void handleSignInResult(@NonNull Task<GoogleSignInAccount> completedTask){
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            authenticate(account.getIdToken());
+            updateUI(account);
+        } catch (ApiException e) {
+            Log.w(TAG, "handleSignInResult:error", e);
+            updateUI(null);
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQ_CODE){
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleResult(result);
-        }
+    private void updateUI(@Nullable GoogleSignInAccount account){
+
     }
+
+    private void authenticate(String googleToken){
+//        HttpClient httpClient = new DefaultHttpClient();
+//        HttpPost httpPost = new HttpPost("pub-hub.us-east-2.elasticbeanstalk.com");
+        //temp assignemnt TODO get token from back end.
+        idToken = googleToken;
+        phbToken = idToken;
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//    }
 
 }
