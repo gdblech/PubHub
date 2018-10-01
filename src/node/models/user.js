6120ad7ce00b31aac69ecb24053a3ca16536a427
Travis Cox <tlcox3@uncg.edu>
2018-09-30 23:10:27 -0400

    #43 Authentication functioning

--- src/node/models/user.js
+++ src/node/models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+const log4js = require('log4js');
+let logger = log4js.getLogger();
 module.exports = (sequelize, DataTypes) => {
 	const User = sequelize.define('User', {
 		userId: {
@@ -22,6 +24,69 @@ module.exports = (sequelize, DataTypes) => {
 		// associations can be defined here
 		User.belongsTo(models.Role);
 		User.belongsTo(models.AuthType);
+	};
+
+	/**
+	 * Updated findOrCreate method that can set Role and AuthType
+	 * @param {*} params 
+	 */
+	User.findOrCreate2 = async (params) => {
+		let user = await User.find({
+			where: params.where
+		});
+
+		if (!user) {
+			const Role = require('../models').Role;
+			const AuthType = require('../models').AuthType;
+			user = await User.create(params.defaults);
+			logger.debug(`built user: ${JSON.stringify(user)}`);
+			logger.debug(params.defaults.role);
+
+			let role = await Role.find({
+				where: {
+					roleName: params.defaults.roleName
+				}
+			});
+
+			let authType = await AuthType.find({
+				where: {
+					provider: params.defaults.authType
+				}
+			});
+
+			user.setRole(role);
+			user.setAuthType(authType);
+		}
+		return user;
+
+	};
+
+	/**
+	 * Get a object with the JWT payload data for the user
+	 */
+	User.prototype.getJWTPayload = function () {
+		let payload = {};
+		payload.userId = this.userId;
+		payload.userName = this.userName;
+		return payload;
+	};
+
+	/**
+	 * return the JSON representation of the user
+	 */
+	User.prototype.getJSON = function () {
+		let json = {};
+		json.userId = this.userId;
+		json.userName = this.userName;
+		json.email = this.email;
+		if (this.profilePicPath) {
+			json.profilePicPath = this.profilePicPath;
+		}
+
+		if (this.Role) {
+			json.role = this.Role.roleName;
+		}
+		return json;
 	};
 	return User;
 };
