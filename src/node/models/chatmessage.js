f9a0c8982f0f675f6591dac6865bbb1db000edbf
Travis Cox <tlcox3@uncg.edu>
2018-10-08 18:16:52 -0400

    undid model changes

--- src/node/models/chatmessage.js
+++ src/node/models/chatmessage.js
@@ -1,12 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const ChatMessage = sequelize.define('ChatMessage', {
-    message: DataTypes.STRING,
-    timestamp: DataTypes.DATE
-  }, {});
-  ChatMessage.associate = function(models) {
-    // associations can be defined here
-    // ChatMessage.belongsTo(models.User);
-  };
-  return ChatMessage;
+	const ChatMessage = sequelize.define('ChatMessage', {
+		message: DataTypes.STRING,
+		timestamp: DataTypes.DATE
+	}, {});
+	ChatMessage.associate = function (models) {
+		// associations can be defined here
+		ChatMessage.belongsTo(models.User);
+	};
+	return ChatMessage;
 };
