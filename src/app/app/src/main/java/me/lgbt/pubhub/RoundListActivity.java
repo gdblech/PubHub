329435d15c68302f6fd5688e2339be6d3616e22a
Geoffrey B <gdblech@uncg.edu>
2018-10-08 18:01:05 -0400

    Added game creation screen

---
+++ src/app/app/src/main/java/me/lgbt/pubhub/RoundListActivity.java
@@ -0,0 +1,13 @@
+package me.lgbt.pubhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoundListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round_list);
+    }
+}
