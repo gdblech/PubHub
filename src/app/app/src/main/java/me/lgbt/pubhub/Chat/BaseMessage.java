da629d7bfaa35f3ec8bde5d8fe4642945a65e3bb
Blair Gentry <bdcollin@uncg.edu>
2018-10-12 22:26:57 -0400

    add chat client server

---
+++ src/app/app/src/main/java/me/lgbt/pubhub/Chat/BaseMessage.java
@@ -0,0 +1,5 @@
+package me.lgbt.pubhub.Chat;
+
+class BaseMessage {
+
+}
---
+++ src/app/app/src/main/java/me/lgbt/pubhub/Chat/MessageEncoder.java
@@ -0,0 +1,26 @@
+package me.lgbt.pubhub.Chat;
+
+import com.google.gson.Gson;
+
+import javax.websocket.EncodeException;
+import javax.websocket.Encoder;
+import javax.websocket.EndpointConfig;
+
+public class MessageEncoder implements Encoder.Text<Message> {
+    private static Gson gson = new Gson();
+
+    @Override
+    public String encode(Message message) throws EncodeException {
+        return gson.toJson(message);
+    }
+
+    @Override
+    public void init(EndpointConfig endpointConfig) {
+        // TODO Custom initialization
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Close resources
+    }
+}
