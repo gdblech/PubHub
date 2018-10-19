49a8977ca9af3c08cbc8b9790fa08f49e79ba889
Blair Gentry <bdcollin@uncg.edu>
2018-10-19 15:59:19 -0400

    change sign in act forward button

--- src/app/app/src/main/java/me/lgbt/pubhub/WSClient/ChatTabActivity.java
+++ src/app/app/src/main/java/me/lgbt/pubhub/WSClient/ChatTabActivity.java
@@ -2,6 +2,9 @@ package me.lgbt.pubhub.WSClient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
 import me.lgbt.pubhub.R;
 
@@ -12,5 +15,22 @@ public class ChatTabActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_tab);
+    }
+
+    /* Displays a message received */
+
+    private class ReceivedMessageHolder extends RecyclerView.ViewHolder {
+        TextView messageText, nameText; // add name and prof pic
+
+        ReceivedMessageHolder(View itemView) {
+            super(itemView);
+            messageText = (TextView) itemView.findViewById(R.id.text_message_body);
+            nameText = (TextView) itemView.findViewById(R.id.text_message_name);
+        }
+
+        void bind(Message message) {
+            messageText.setText(message.getMessage());
+
+        }
     }
 }
