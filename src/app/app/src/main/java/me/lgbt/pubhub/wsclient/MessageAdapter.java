354f4b3e88502a0599b95a8229e95db9baf0ca20
Blair Gentry <bdcollin@uncg.edu>
2018-10-25 12:34:38 -0400

    message classes cleaning

---
+++ src/app/app/src/main/java/me/lgbt/pubhub/wsclient/MessageAdapter.java
@@ -0,0 +1,49 @@
+package me.lgbt.pubhub.wsclient;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageAdapter extends BaseAdapter {
+    List<Message> messages = new ArrayList<Message>();
+    Context context;
+
+    public MessageAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void add(Message message) {
+        this.messages.add(message);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return messages.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return messages.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        MessageViewHolder holder = new MessageViewHolder();
+        LayoutInflater messageInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        Message message = messages.get(i);
+
+
+    }
+}
