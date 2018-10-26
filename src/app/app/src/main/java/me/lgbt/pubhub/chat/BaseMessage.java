7f0dc29fa5ba14eff092f679d022bf1ea0c494c3
Blair Gentry <bdcollin@uncg.edu>
2018-10-26 15:19:27 -0400

    update chat, remove wsclient

---
+++ src/app/app/src/main/java/me/lgbt/pubhub/chat/BaseMessage.java
@@ -0,0 +1,16 @@
+package me.lgbt.pubhub.chat;
+
+import me.lgbt.pubhub.users.User;
+
+public class BaseMessage {
+
+    public class UserMessage {
+
+
+
+        public User getSender() {
+            return null;
+        }
+    }
+
+}
--- src/app/app/src/main/java/me/lgbt/pubhub/chat/MessageListActivity.java
+++ src/app/app/src/main/java/me/lgbt/pubhub/chat/MessageListActivity.java
@@ -2,14 +2,23 @@ package me.lgbt.pubhub.chat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import me.lgbt.pubhub.R;
 
 public class MessageListActivity extends AppCompatActivity {
+    private RecyclerView mMessageRecycler;
+    private MessageListAdapter mMessageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_list);
+
+        mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
+        mMessageAdapter = new MessageListAdapter(this, messageList);
+        mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
+        //mMessageRecycler.setAdapter(mMessageAdapter);
     }
 }
