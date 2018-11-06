2c29fc961499e8ea0e9e8ba432403d8e7d5ab5b4
Geoffrey B <gdblech@uncg.edu>
2018-11-06 13:42:47 -0500

    Needs work on time

--- src/app/app/src/main/java/me/lgbt/pubhub/chat/UserMessage.java
+++ src/app/app/src/main/java/me/lgbt/pubhub/chat/UserMessage.java
@@ -1,6 +1,8 @@
 package me.lgbt.pubhub.chat;
 
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 public class UserMessage extends Message{
@@ -13,6 +15,12 @@ public class UserMessage extends Message{
         this.sender = sender;
         this.text = text;
         this.time = time;
+    }
+
+    public UserMessage(String text, String sender, String timeStamp){
+        this.sender = sender;
+        this.text = text;
+        this.time = convertTime(timeStamp);
     }
 
     public String getText() {
@@ -46,6 +54,13 @@ public class UserMessage extends Message{
     public String getTimeString(){
         SimpleDateFormat sdf = new SimpleDateFormat("H:mm", Locale.US);
         return sdf.format(time);
+    }
+
+    private long convertTime(String timeStamp){
+//        timeStamp = timeStamp.replace("Z", "");
+//        Timestamp stamp = Timestamp.valueOf(timeStamp);
+//        return stamp.getTime() / 1000L;
+        return 0;
     }
 
 }
