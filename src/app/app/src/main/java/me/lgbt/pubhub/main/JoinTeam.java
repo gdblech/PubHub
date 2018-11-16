f5a631c2904335d6dbea0d5eff22fac2bb552707
Linh Tran <l_tran2@uncg.edu>
2018-11-16 16:09:55 -0500

    add create_team, join_team fragments

---
+++ src/app/app/src/main/java/me/lgbt/pubhub/main/JoinTeam.java
@@ -0,0 +1,30 @@
+package me.lgbt.pubhub.main;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.lgbt.pubhub.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class JoinTeam extends Fragment {
+
+
+    public JoinTeam() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_join_team, container, false);
+    }
+
+}
