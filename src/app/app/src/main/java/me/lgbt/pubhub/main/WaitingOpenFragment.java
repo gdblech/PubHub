eca3ae3612ffb3fe810f4b1c6343de4014f50dd5
Linh Tran <l_tran2@uncg.edu>
2018-11-16 16:09:55 -0500

    add create_team, join_team fragments

--- src/app/app/src/main/java/me/lgbt/pubhub/main/WaitingOpenFragment.java
+++ src/app/app/src/main/java/me/lgbt/pubhub/main/WaitingOpenFragment.java
@@ -1,5 +1,7 @@
 package me.lgbt.pubhub.main;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,22 +10,52 @@ import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
 
 import me.lgbt.pubhub.R;
+import me.lgbt.pubhub.trivia.start.QRCodeScanner;
 
-public class WaitingOpenFragment extends Fragment {
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class WaitingOpenFragment extends Fragment implements View.OnClickListener {
+    EditText message;
+    private String welcomeMesg;
+    private Button btnToChat;
+    private Button btnToQR;
+    private boolean isGame;
 
+
+    public WaitingOpenFragment(){
+        //Empty constructor
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_waiting_open, container, false);
+        View v = inflater.inflate(R.layout.fragment_waiting_open, container, false);
+        btnToQR = (Button) v.findViewById(R.id.btnToQR);
+        btnToChat = (Button) v.findViewById(R.id.btnToChat);
+        message = (EditText) v.findViewById(R.id.waitingOpenText);
+
+        //if there is game scan QR_code to join a table
+        isGame = isGameAvail();
+        if(isGame){
+            btnToChat.setVisibility(View.VISIBLE);
+            btnToQR.setVisibility(View.GONE);
+            message.setText(welcomeMesg);
+
+
+        } else {
+            btnToChat.setVisibility(View.GONE);
+            btnToQR.setVisibility(View.VISIBLE);
+            message.setText(welcomeMesg);
+        }
+
+        return v;
     }
 
     @Override
@@ -33,4 +65,37 @@ public class WaitingOpenFragment extends Fragment {
         assert act != null;
         TextView text = act.findViewById(R.id.waitingOpenText);
     }
+
+    public void onClick(View view){
+
+    }
+
+    //open qr_code activity
+    public void openQR(){
+        Intent i = new Intent(getActivity(), QRCodeScanner.class);
+        startActivity(i);
+    }
+
+
+    //if game available
+    //if comes from the messages from the server
+    public boolean isGameAvail(){
+        isGame = false;
+        if ( true ){ // TODO: 11/16/2018 : handle this later. True is only for testing //>
+            isGame = true;
+        }
+        return isGame;
+    }
+
+    public String welcomeMeg(){
+        if (isGame = !true){
+            welcomeMesg = " No game vailable!";
+            }
+            else{
+            welcomeMesg = " welcome back";
+        }
+        return welcomeMesg;
+    }
+
+
 }
