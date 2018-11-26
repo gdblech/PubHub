f75138f1883652fe9c3e9841d89ae0f008514422
Linh Tran <l_tran2@uncg.edu>
2018-11-25 20:03:53 -0500

    close the keyboard after hitting the submit answer

--- src/app/app/src/main/java/me/lgbt/pubhub/main/PlayFragment.java
+++ src/app/app/src/main/java/me/lgbt/pubhub/main/PlayFragment.java
@@ -10,6 +10,7 @@ import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -94,9 +95,16 @@ public class PlayFragment extends Fragment implements View.OnClickListener {
                 passer.answerClicked(answer.getText().toString());
                 answer.setVisibility(View.GONE);
                 answer.setText("");
-
+                closeKeyboard();
                 submitAnswer.hide();
             }
         }
+    }
+
+    private void closeKeyboard(){
+        View view = getActivity().getCurrentFocus();
+        if (view != null){
+            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);        }
     }
 }
